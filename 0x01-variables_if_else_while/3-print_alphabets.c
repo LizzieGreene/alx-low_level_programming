adada64bd61dda4229ba3c0bec922c92cbdb9204
LizzieGreene <nafulaelizabeth62@gmail.com>
2023-09-08 16:17:31 -0700

    Print all alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 'a';
+for (x = 'a'; x <= 'z'; x++)
+{
+int lower_x = x;
+putchar(lower_x);
+}
+for (x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = x;
+putchar(lower_x);
+}
+putchar('\n');
+return (0);
+}
