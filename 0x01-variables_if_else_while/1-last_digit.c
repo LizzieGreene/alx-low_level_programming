5d251de1feed476f2652af71d37e4fbaf94f3af4
LizzieGreene <nafulaelizabeth62@gmail.com>
2023-09-08 15:48:23 -0700

    If else statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (abs(n % 10) == 0)
 {
-printf("Last digit of %d is 0 and is zero\n", n);
+printf("Last digit of %d is 0 and is 0\n", n);
 }
 else if ((n % 10) > 5)
 {
