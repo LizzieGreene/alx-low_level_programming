12fda2de31473b8c7ce0807c8b7192890c8ab0c6
LizzieGreene <nafulaelizabeth62@gmail.com>
2023-09-12 13:58:28 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - print_alphabet function description
+ * @c: parameter passed
+ * Return: 1 if islower is not
+ */
+int print_last_digit(int c)
+{
+return (abs(c % 10));
+}
