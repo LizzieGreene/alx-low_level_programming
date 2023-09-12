a841f246ba686ff251f6722040ed8399603ad2ba
LizzieGreene <nafulaelizabeth62@gmail.com>
2023-09-12 12:37:09 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - print_alphabet function description
+ */
+void print_alphabet(void)
+{
+char x = 'a';
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
