532ae3e639da23a38ce3640f8c6c68615d426d2c
LizzieGreene <nafulaelizabeth62@gmail.com>
2023-09-12 13:47:22 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - print_alphabet function description
+ * @c: parameter passed
+ * Return: 1 if islower is not
+ */
+int _abs(int c)
+{
+return (abs(c));
+}
